1fd4919089165040402822e88e50cb5f711d2703
Fatkharrofiqi <achmadfatkharrofiqi404@gmail.com>
2025-05-12 14:31:24 +0700

    fix: async to detail country page

--- src/app/countries/[id]/page.tsx
+++ src/app/countries/[id]/page.tsx
@@ -1,9 +1,7 @@
 import DetailCountry from "@/components/page/countries/detail";
 
 interface PageProps {
-  params: {
-    id: string;
-  };
+  params: Promise<{ id: string }>;
 }
 
 export default async function Page({ params }: PageProps) {
